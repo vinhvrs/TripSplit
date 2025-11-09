1cd949373644f53b97978029e4be09739eae5387
Thanh Thuy Huynh <hthuy03.work@gmail.com>
2025-11-09 18:26:52 +0700

    feat: update cors whitelist domains
    Included front-end domains into cors whitelist to be able to fetch data from endpoint. please review this implementation

--- backend/src/utils/constants.js
+++ backend/src/utils/constants.js
@@ -1,1 +1,7 @@
-export const WHITELIST_DOMAINS = 'http://localhost:3000'
+export const WHITELIST_DOMAINS = [
+  "http://localhost:5173", // Vite dev server (default)
+  "http://localhost:3000", // React/Next.js dev server
+  "http://localhost:5174", // Vite alternative port
+  "http://localhost:8080", // Vue CLI dev server
+  "http://127.0.0.1:5173", // Alternative localhost (IPv4)
+];
